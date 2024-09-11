42a75def31cb008469407ea77c20658ed10ba8a5
Szymon22sito <szymon.sitarczukss@wp.pl>
2024-09-11 11:41:01 +0200

    wypisywanie z tablicy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //tablica
+        //musi miec rozmiar
+        //nie mozna potem zmienic
+        //elementy proste i zlozone
+        //wartosci losowe od 1 do 100
+        //6 elemen
+        int [] tablicaLosowych = new int[6];
+        for (int i = 0; i < tablicaLosowych.length; i++) {
+            tablicaLosowych[i] = (int) (Math.random()*100+1);
+        }
+        //wypisywanie elementów z tablicy
+        for (int element:tablicaLosowych) {
+            System.out.println(element+", ");
+        }
     }
+
+
+
 }
