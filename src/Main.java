6df69ab975ffec9a4e3f8fe782c38338dfa38d8b
Szymon22sito <szymon.sitarczukss@wp.pl>
2024-09-11 13:35:33 +0200

    metody

--- src/Main.java
+++ src/Main.java
@@ -65,4 +65,47 @@ public class Main {
         System.out.println("Trafione:"+trafione);
 
     }
-}
+    //metody
+
+    /**
+     * Metoda wylosujLiczby losuje liczby calkowite z zakresu 1 do 100
+     * @param ile - liczba całkowita przechowująca ile liczb wylosujemy
+     * @return lista z wylosoanymi liczbami
+     */
+    private ArrayList<Integer> wylosujLiczby(int ile) {
+        ArrayList<Integer>listaLosowychBezpow = new ArrayList<>();
+        for (int i = 0; i < ile; i++){
+            int liczba = (int) (Math.random()*100+1);
+            while (listaLosowychBezpow.contains(liczba)){
+                liczba = (int) (Math.random()*100+1);
+            }
+            listaLosowychBezpow.add(liczba);
+        }
+        return listaLosowychBezpow;
+    }
+    private ArrayList<Integer> wpiszLiczbyZKlawiatury(int ile){
+        ArrayList<Integer> listaLiczbWpisanych = new ArrayList<>();
+        System.out.println("podaj"+ile+"liczb");
+        Scanner klawiatura = new Scanner(System.in);
+        for (int i = 0; i < ile; i++) {
+            int liczba = klawiatura.nextInt();
+            listaLiczbWpisanych.add(liczba);
+        }
+        return listaLiczbWpisanych;
+    }
+    private  void  wypiszKolekcjie(List<Integer> listaDoWpisania){
+    for (Integer element:listaDoWpisania) {
+        System.out.println("Element:" + element);
+        }
+    }
+    private LinkedList<Integer> zwrocTrafione(ArrayList<Integer> listaLiczbWpisanych,
+                                              ArrayList<Integer>listaLiczbLosowych){
+        LinkedList<Integer> trafione =new LinkedList<>();
+        //trafiione to elementy które występują w wylosowanych i wpisanych
+        for (Integer wpisana: listaLiczbWpisanych) {
+            if(listaLiczbLosowych.contains(wpisana)){
+                trafione.add(wpisana);
+            }
+        }
+        return trafione;
+}}
