577220c716c6f365ef576e596da7c79cb8904d15
Szymon22sito <szymon.sitarczukss@wp.pl>
2024-09-12 07:33:59 +0200

    trafiono

--- src/Main.java
+++ src/Main.java
@@ -45,7 +45,8 @@ public class Main {
             }
         }
         return trafione;
-        public static void main(String[] args) {
+        public static void main(String[]Object args;
+        args) {
             ArrayList<Integer> losowe = wylosujLiczby( 6);
             System.out.println("wylosowana lista");
             wypiszKolekcjie(losowe);
